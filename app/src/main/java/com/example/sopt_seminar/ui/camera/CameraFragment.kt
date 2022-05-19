ffa6514e9a8d0cdfb44a6e135d8d2d11886d75b6
KWY0218 <wodrk1001@gmail.com>
2022-05-19 17:29:58 +0900

    [FEAT] 세미나3 도전과제 추가

--- app/src/main/java/com/example/sopt_seminar/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/sopt_seminar/ui/camera/CameraFragment.kt
@@ -1,9 +1,45 @@
 package com.example.sopt_seminar.ui.camera
 
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
+import androidx.core.content.ContextCompat
+import coil.load
 import com.example.sopt_seminar.R
 import com.example.sopt_seminar.databinding.FragmentCameraBinding
 import com.example.sopt_seminar.util.BaseFragment
+import com.example.sopt_seminar.util.CustomContract
 
 
 class CameraFragment : BaseFragment<FragmentCameraBinding>(R.layout.fragment_camera) {
+    private val activityResultLauncher =
+        registerForActivityResult(CustomContract.GetPhoto()) { uri ->
+            if (uri != null) binding.cameraImg.load(uri)
+        }
+    
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                activityResultLauncher.launch()
+            } else {
+                if (ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) activityResultLauncher.launch()
+            }
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.cameraBtn.setOnClickListener {
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
 }
