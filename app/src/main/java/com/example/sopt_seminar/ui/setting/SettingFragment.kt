c3fe86e3831520f5ee14077c17292f37b545d9ac
KWY0218 <wodrk1001@gmail.com>
2022-06-10 09:56:03 +0900

    [FEAT] 자동로그인 해제 기능

--- app/src/main/java/com/example/sopt_seminar/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/sopt_seminar/ui/setting/SettingFragment.kt
@@ -3,14 +3,18 @@ package com.example.sopt_seminar.ui.setting
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.sopt_seminar.R
 import com.example.sopt_seminar.databinding.FragmentSettingBinding
 import com.example.sopt_seminar.domain.usecase.SetAutoLoginUseCase
 import com.example.sopt_seminar.util.BaseFragment
+import com.example.sopt_seminar.util.BaseToast.showToast
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
     @Inject
     lateinit var setAutoLoginUseCase: SetAutoLoginUseCase
@@ -20,10 +24,12 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_s
 
         binding.settingImgBtn.setOnClickListener {
             MaterialAlertDialogBuilder(requireContext())
-                .setMessage("자동로그인 해제?")
-                .setPositiveButton("해제") { _, _ ->
+                .setMessage(resources.getString(R.string.setting_dialog_text))
+                .setPositiveButton(resources.getString(R.string.setting_dialog_positive)) { _, _ ->
                     lifecycleScope.launch {
+                        requireContext().showToast(resources.getString(R.string.setting_auto_login))
                         setAutoLoginUseCase(false)
+                        findNavController().navigateUp()
                     }
                 }
                 .show()
